87c786361f02858c44f8123a6ed9acf2db11813f
zhouxuchi <zxuchi@163.com>
2016-12-14 17:55:53 +0800

    修改宽度超出bug，添加备注

--- yanxuan/Gruntfile.js
+++ yanxuan/Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     // grunt.loadNpmTasks('grunt-contrib-concat');
     // grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // grunt.loadNpmTasks('grunt-contrib-connect');
     // grunt.loadNpmTasks('grunt-contrib-cssmin');
     // grunt.loadNpmTasks('grunt-contrib-compass');
